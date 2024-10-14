1f93ac6be96bdbd6148115529ef33f43c3e073d2
rikut03 <yamariku0820@outlook.jp>
2024-10-14 14:35:35 +0900

    デプロイ修正

--- config/google.php
+++ config/google.php
@@ -1,6 +1,7 @@
 <?php
 return
 [
-    'google_credential' => env('GOOGLE_APPLICATION_CREDENTIAL'),
+    // 'google_credential' => env('GOOGLE_APPLICATION_CREDENTIAL'),
     'voicevox_url' => env('VOICEVOX_URL'),
+    'google_credential' => json_decode(env('GOOGLE_APPLICATION_CREDENTIAL'), true),
 ];
